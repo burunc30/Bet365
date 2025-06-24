b245944aeb40539f4c615fc4828477ca1b0759be
burunc30 <e.necefov1985@gmail.com>
2025-06-24 20:22:53 +0400

    Update stealth.js

--- stealth.js
+++ stealth.js
@@ -1,10 +1,1 @@
-// stealth.js
-Object.defineProperty(navigator, 'webdriver', { get: () => false });
-Object.defineProperty(navigator, 'languages', { get: () => ['en-US', 'en'] });
-Object.defineProperty(navigator, 'plugins', { get: () => [1, 2, 3] });
-
-window.chrome = {
-  runtime: {}
-};
-
-Object.defineProperty(navigator, 'platform', { get: () => 'Win32' });
+Object.defineProperty(navigator, 'webdriver', { get: () => undefined })
